0a1277f6828fa541fae423bf40439097de19b4f2
ayoubga <42929812+ayoubga@users.noreply.github.com>
2018-10-21 12:02:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -889,18 +889,8 @@ message.channel.stopTyping()
 })
 }
 });
+ 
 
-client.login(process.env.BOT_TOKEN);
-
-
-const Discord = require('discord.js');
-const moment = require("moment");  
-const fs = require("fs");      
-const dateFormat = require('dateformat');
-const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
-const prefix = "#"
-let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 ///////////////////////////////////
 
